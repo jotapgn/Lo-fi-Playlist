e5183dc4a23af9c978dec370d542f032fbdf3ff9
jotapgn <joaopedrogomes26@gmail.com>
2020-08-07 11:35:55 -0300

    Update Page

--- src/pages/Page404/index.js
+++ src/pages/Page404/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NotFound from '../../components/NotFound'
-import foto from '../../assets/img/405.png'
+// import foto from '../../assets/img/405.png'
 import PageDefault from '../../components/PageDefault'
 
 function Page404(){

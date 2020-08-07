94d0ae83f352292a8ab0f3fe2a0df13e08d13eb5
jotapgn <joaopedrogomes26@gmail.com>
2020-08-07 11:02:50 -0300

    Add Components

---
+++ src/components/BgNotFound/index.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import foto from '../../assets/img/405.png'
+
+
+const BgNotFound = styled.img.attrs({src: foto, alt: 'Logo Lo-FI PLaylist'})`
+@media (max-width: 800px) {
+  max-width: 105px;
+}
+
+`
+
+export default BgNotFound;
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import BgNotFound from '../BgNotFound'
+import foto from '../../assets/img/405.png'
+
+const NotFound = styled.div`
+background-image: url(${foto});
+position: absolute;
+width: 100%;
+height: 100%;
+background-size: cover;
+ 
+`
+
+export default NotFound;

cf8bf03a160e7a4d01106fa9e1e28ec6171b9a12
jotapgn <joaopedrogomes26@gmail.com>
2020-08-07 11:00:07 -0300

    Update Index Js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './styles/settings/global.css'
 import Home from './pages/Home';
 import Video from './pages/cadastro/Video';
 import Categoria from './pages/cadastro/Categoria'
+import Page405 from './pages/Page404'
 
 import { BrowserRouter, Switch, Route } from  'react-router-dom';
 
-const Page404 = () => (<div>Página 404</div>)
+
 
 ReactDOM.render(
   <BrowserRouter>
@@ -16,7 +17,7 @@ ReactDOM.render(
     <Route path="/cadastro/video" component={Video} />
     <Route path="/cadastro/categoria" component={Categoria} />
     <Route path="/" component={Home} exact />
-    <Route component={Page404} />
+    <Route component={Page405} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')

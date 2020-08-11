f34e259dabc8b76d25a551cb1d4df5eee1101333
jotapgn <joaopedrogomes26@gmail.com>
2020-08-11 16:25:01 -0300

    Add hooks

---
+++ src/hooks/useForm.js
@@ -0,0 +1,30 @@
+import { useState} from 'react';
+
+
+function useForm(valoresIniciais){
+    const [values, setValues] = useState(valoresIniciais);
+    function setValue(chave, valor){
+        setValues({
+            ...values,
+            [chave]: valor,
+        })
+    }
+    function handleChange(eventInfo) {
+        setValue(
+            eventInfo.target.getAttribute('name'),
+            eventInfo.target.value
+        )
+    }
+
+    function clearForm(){
+        setValues(valoresIniciais)
+    }
+
+    return{
+        values,
+        handleChange,
+        clearForm,
+    }
+} 
+
+export default useForm;
